99b7da2debf58eabd7d12c4b08efcfdf253e38ac
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-05-20 23:35:06 +0530

    Sort using bubble algorithm

---
+++ 3.Sorting/bubbleSort.js
@@ -0,0 +1,21 @@
+//Problem Statement: Given an array of N integers, write a program to implement the Bubble Sorting algorithm.
+function bubbleSort(inputArray) {
+    //Start from the end of array
+    for(let j = inputArray.length-1; j > 0; j--) {
+        //Start from begining till the j
+        for(let i =0; i< j;i++) {
+            if(inputArray[i] > inputArray[i+1]) {
+                //Swap
+                let temp = inputArray[i];
+                inputArray[i] = inputArray[i+1];
+                inputArray[i+1] = temp;
+            }
+            
+        }
+    }
+    
+    return inputArray;
+}
+
+let inputArray = [ 78, 12, 9, 6, 5, 55 ];
+console.log(bubbleSort(inputArray));
