3e4e4878038deb44e601e02d5734eaf52400780f
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-05-31 23:27:32 +0530

    Find largest element

---
+++ 4.Arrays_Easy/largestElement.js
@@ -0,0 +1,14 @@
+//Problem Statement: Given an array, we have to find the largest element in the array.
+
+function findLargestElement(array) {    
+    let max = Number.MIN_SAFE_INTEGER;
+    array.forEach(element => {
+        if(element > max) {
+            max = element;
+        }
+    });   
+    return max;
+}
+
+let inputArray = [ 78, 12, 9, 6, 5, 55 ];
+console.log(findLargestElement(inputArray));
