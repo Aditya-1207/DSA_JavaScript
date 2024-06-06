61f009ea1925ff1a47f9b793c3bfda09a9616673
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-06-06 23:25:22 +0530

    move zero to end optimised

--- 4.Arrays_Easy/moveZerosToEnd.js
+++ 4.Arrays_Easy/moveZerosToEnd.js
@@ -3,19 +3,44 @@
 
 function moveZerosToEnd(array) {
     //1.Brute force : Time and Space Complexity : O(n)
-    let zeroIndexArray = [];
+    // let zeroIndexArray = [];
+    // for (let index = 0; index < array.length; index++) {
+    //     if(array[index] != 0){
+    //         zeroIndexArray.push(array[index]);
+    //     }        
+    // }
+    
+    // let length = array.length-zeroIndexArray.length;
+    // for (let index = 0; index < length; index++) {
+    //     zeroIndexArray.push(0);   
+    // }
+    // return zeroIndexArray;
+
+    //2.Optimised approach with in-place movment of array elements
+    //2 pointer approach
+    let zeroIndex = 0;
+    
     for (let index = 0; index < array.length; index++) {
-        if(array[index] != 0){
-            zeroIndexArray.push(array[index]);
+        if(array[index] == 0){
+            zeroIndex = index;
+            break;
         }        
     }
-    
-    let length = array.length-zeroIndexArray.length;
-    for (let index = 0; index < length; index++) {
-        zeroIndexArray.push(0);   
+    let nonZeroIndex =zeroIndex+1;   
+    while(zeroIndex < array.length-1 && nonZeroIndex < array.length){
+        if(array[zeroIndex] == 0 && array[nonZeroIndex] != 0) {      
+            let temp = array[zeroIndex];      l
+            array[zeroIndex] = array[nonZeroIndex];
+            array[nonZeroIndex] = temp;
+            zeroIndex++;
+        }
+        nonZeroIndex++;
     }
-    return zeroIndexArray;
+    return array;
+
 }
 
-let inputArray = [ 0,0,1 ];
-console.log(moveZerosToEnd(inputArray));
+//let inputArray =  [1, 0, 2, 3, 2, 0, 0, 4, 5, 1];
+let inputArray = [2,1];
+moveZerosToEnd(inputArray);
+console.log(inputArray);
