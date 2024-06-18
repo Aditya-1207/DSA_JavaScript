91b5819d2f25ba8242cdc1497738a3ed48adafb5
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-06-19 00:00:37 +0530

    Optimised approch with Dutch flag

--- 5.Arrays_Medium/sortArrayof012.js
+++ 5.Arrays_Medium/sortArrayof012.js
@@ -5,34 +5,59 @@
 function sortArrayof012 (array) {
 
     //1.Brute force
-    let zeroCount = 0;
-    let oneCount = 0;
-    let twoCount = 0;   
+    // let zeroCount = 0;
+    // let oneCount = 0;
+    // let twoCount = 0;   
 
-    array.forEach(element => {
-        if(element == 0){
-            zeroCount++;
-        } else if(element == 1){
-            oneCount++;
-        } else{
-            twoCount++;
-        }
-    });
+    // array.forEach(element => {
+    //     if(element == 0){
+    //         zeroCount++;
+    //     } else if(element == 1){
+    //         oneCount++;
+    //     } else{
+    //         twoCount++;
+    //     }
+    // });
 
-    for(let i=0;i<zeroCount; i++) {
-        array[i]=0;
-    }
-    for(let i=zeroCount;i<zeroCount+oneCount; i++) {
-        array[i]=1;
-    }
-    for(let i=zeroCount+oneCount;i<zeroCount+oneCount+twoCount; i++) {
-        array[i]=2;
-    }
+    // for(let i=0;i<zeroCount; i++) {
+    //     array[i]=0;
+    // }
+    // for(let i=zeroCount;i<zeroCount+oneCount; i++) {
+    //     array[i]=1;
+    // }
+    // for(let i=zeroCount+oneCount;i<zeroCount+oneCount+twoCount; i++) {
+    //     array[i]=2;
+    // }
+    //2.Optimised appraoch: Dutch flag
+    let left =0;
+    let mid = 0;
+    let right = array.length-1;
 
+    while (mid <= right) {
+        console.log("array[mid] is ",array[mid]);
+        if(array[mid] == 0) {
+            console.log("inside 0");
+            swap(array,left,mid);  
+            left++;
+            mid++;
+        } else if(array[mid] == 1) {
+            console.log("inside 1");
+            mid++;
+        } else {
+            console.log("inside 2");
+            swap(array,mid,right);            
+            right--;
+        }        
+    }
     return array;
-
 }
 
-let array = [2,0,2,1,1,0];
+function swap(array,start,end) {
+    let temp = array[start];
+    array[start] = array[end];
+    array[end] = temp;
+}
+
+let array = [2,0,2,1,1,2];
 
 console.log(sortArrayof012(array));
