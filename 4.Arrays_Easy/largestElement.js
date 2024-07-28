82f53b16a3e30afd9c0f67ec92c270d2b9abbfa3
Aditya-1207 <108715826+Aditya-1207@users.noreply.github.com>
2024-07-29 00:03:03 +0530

    Using reduce to find max element

--- 4.Arrays_Easy/largestElement.js
+++ 4.Arrays_Easy/largestElement.js
@@ -1,14 +1,24 @@
 //Problem Statement: Given an array, we have to find the largest element in the array.
 
-function findLargestElement(array) {    
-    let max = Number.MIN_SAFE_INTEGER;
-    array.forEach(element => {
-        if(element > max) {
-            max = element;
-        }
-    });   
-    return max;
-}
+// function findLargestElement(array) {    
+//     let max = Number.MIN_SAFE_INTEGER;
+//     array.forEach(element => {
+//         if(element > max) {
+//             max = element;
+//         }
+//     });   
+//     return max;
+// }
+
+
 
 let inputArray = [ 78, 12, 9, 6, 5, 55 ];
-console.log(findLargestElement(inputArray));
+//console.log(findLargestElement(inputArray));
+
+//Use of Reduce to find solution
+console.log(inputArray.reduce(function(acc, current){
+    if(current > acc){
+        acc = current;
+    }
+    return acc;
+},Number.MIN_SAFE_INTEGER));
